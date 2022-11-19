e9507b8f8f673e7589915f1fadcac0c0922ee0eb
angel charlot <angelcm18205@gmail.com>
2022-11-19 19:47:21 +0100

    detalles

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,9 +1,9 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+@extends('layouts/app')
+@section('body')
+    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Profile') }}
-        </h2>
-    </x-slot>
+    </h2>
+
 
     <div>
         <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
@@ -40,4 +40,4 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+    @endsection
