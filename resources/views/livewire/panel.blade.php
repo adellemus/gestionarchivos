6a7d6a3966b4f128ac749a50f9108d2c5c3893ed
angel charlot <angelcm18205@gmail.com>
2022-12-03 01:37:37 +0100

    dsfdsf

--- resources/views/livewire/panel.blade.php
+++ resources/views/livewire/panel.blade.php
@@ -53,7 +53,6 @@
                 @break
 
                 @default
-<<<<<<< HEAD
                     <div
                         class="w-10/12 mx-auto flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center  sm:pt-0">
 
@@ -73,24 +72,6 @@
                                                 <img class=" rounded-full bg-cover " src="{{ asset('imagenes/adel.jpg') }}"
                                                     alt="">
                                             </div>
-=======
-             
-
-
-                    <div class="w-10/12  mx-auto sm:px-6 lg:px-8 p-5">
-            
-                        <div class="w-full text-3xl text-center font-bold">
-                            Software de gestión documental
-                        </div>
-            
-                        <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
-                            <div class="grid grid-cols-1 md:grid-cols-2">
-            
-                                <div class="p-5">
-                                    <div class="flex-shrink-0 flex items-center">
-                                        <div class="w-1/5 rounded-full mx-auto ">
-                                            <img class=" rounded-full bg-cover "  src="{{asset('imagenes/adel.jpg')}}" alt="">
->>>>>>> master
                                         </div>
                                         <div class="p-2 text-justify">Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                                             Expedita tempore ad perferendis nostrum natus porro quam repellat, aperiam
@@ -118,10 +99,6 @@
 
                         </div>
                     </div>
-<<<<<<< HEAD
-=======
-                
->>>>>>> master
             @endswitch
 
 
